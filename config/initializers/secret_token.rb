3066bbca8b247e1088e435a0409ce0f9399305b3
himajin315 <himajin315@gmail.com>
2014-03-13 10:16:48 +0900

    rails new palmistry

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palmistry::Application.config.secret_key_base = '4e0e6b66cf764b2d751d23896ec787e17bb914189e6efacbf29a1666bea61050c58bcc6145d0d4301b5e5886e0309b970b411db3b5ea7f1a3bcf44f1e1b73edf'
