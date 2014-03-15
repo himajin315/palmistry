b2ca9120391b6ee5d4d26aec3fd3fc79d3d4f9bb
yoshiaki <himajin315@gmail.com>
2014-03-15 18:34:27 +0900

    POSTメソッドを使ってみた。

--- app/controllers/palmistry_controller.rb
+++ app/controllers/palmistry_controller.rb
@@ -1,8 +1,11 @@
 class PalmistryController < ApplicationController
+  skip_before_filter :verify_authenticity_token ,:only=>[:result]
+
   def appraisal
   end
 
   def result
+    render :text => "旅行先 = #{params[:dest]}, 人数 = #{params[:num]}人"
   end
 
   def profile
