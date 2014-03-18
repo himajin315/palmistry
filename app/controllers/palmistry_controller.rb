cd375400a3fd35b823910d044e3377294cd47100
yoshiaki <himajin315@gmail.com>
2014-03-19 00:18:48 +0900

    controller側で表示する文字を交換する

--- app/controllers/palmistry_controller.rb
+++ app/controllers/palmistry_controller.rb
@@ -5,11 +5,50 @@ class PalmistryController < ApplicationController
   end
 
   def result
-    @love = params[:love]
+    case params[:love]
+    when '0' then
+      @love = "あ";
+    when '1' then
+      @love = "い";
+    when '2' then
+      @love = "う";
+    when '3' then
+      @love = "え";
+    when '4' then
+      @love = "お";
+    end
+
     @love_etc = params[:love_etc]
+
     @life = params[:life]
-    @life_etc = params[:life_etc]
-    @brain = params[:brain]
+    case params[:life]
+    when '0' then
+      @life = "か";
+    when '1' then
+      @life = "き";
+    when '2' then
+      @life = "く";
+    end
+
+    case params[:life_etc]
+    when '0' then
+      @life_etc = "さ";
+    when '1' then
+      @life_etc = "し";
+    when '2' then
+      @life_etc = "す";
+    end
+
+    case params[:brain]
+    when '0' then
+      @brain = "た";
+    when '1' then
+      @brain = "ち";
+    when '2' then
+      @brain = "つ";
+    when '3' then
+      @brain = "て";
+    end
   end
 
   def profile
