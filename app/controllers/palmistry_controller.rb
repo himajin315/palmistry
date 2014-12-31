8306eb44e9a08dbdff826a2c404288402195b96b
himajin315 <himajin315@gmail.com>
2015-01-01 03:13:20 +0900

    Add sun_line.rb sun を controller から modele に移動

--- app/controllers/palmistry_controller.rb
+++ app/controllers/palmistry_controller.rb
@@ -53,39 +53,17 @@ class PalmistryController < ApplicationController
       @jupiter_reader = jupiter_line.result_reader(jupiter)
     end
 
-    case params[:sun]
-    when '0' then
-      @sun = @uname + " さんは地道な努力で出世するタイプです。<br>
-      「継続は力なり」がピッタリな人です。<br>
-      続けることで周りから認められる存在となります。<br>
-      うまくいなかいからと言ってすぐに諦めるのではなく、根気よく続けていくことが大事になってきます。<br>
-      そうすることで、地位や名声を得られることでしょう。<br>"
-    when '1' then
-      @sun = @uname + " さんは才能を活かして出世するタイプです。<br>
-      自分の才能を活かせる場にいるかどうかで、出世ができるかどうかが決まってきます。<br>
-      職業や場所がとても重要な要素でしょう。<br>
-      自分のやりたいこととできることは違います。<br>
-      やりたいことも大事ですが、才能にあった場所を見つけることも大事です。<br>
-      " + @uname + " さんを必要としている場所に行くことでお金には困らない生活をすることでしょう。<br>"
-    when '2' then
-      @sun_not = "ただ、" + @uname + " さんは現在才能を活かしきれていません。<br>
-      今やりたいことはありますか。<br>
-      " + @uname + " さんの才能に合ったことがうまくできていません。<br>
-      今やっていること辞めろってわけではありません。<br>
-      今やっていることに" + @uname + " さんの才能を活かすことが必要になってきます。<br>"
+    sun_line = SunLine.new(@uname,@sex_human)
+    if params[:sun] == '2' then
+      @sun_skill_until = sun_line.result_skill(params[:sun])
+    else
+      @sun_skill = sun_line.result_skill(params[:sun])
+    end
+    sun_other = params[:sun_other]
+    if sun_other.present? then
+      @sun_charm = sun_line.result_charm(sun_other)
     end
 
-    sun_other = params[:sun_other]
-    if sun_other then
-      sun_other.keys.each do |select_num|
-        case select_num
-        when '0' then
-          @sun_other_charm = @uname + " さんは魅力的な人でもあります。<br>
-          異性から好かれやすいようです。<br>
-          所謂、モテる人でしょう。<br>"
-        end
-      end
-    end
 
     case params[:mercury]
     when '0' then
