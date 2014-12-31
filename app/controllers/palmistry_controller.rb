63fd70ab3c01e0f26e8d221c022b61c328161f6f
himajin315 <himajin315@gmail.com>
2015-01-01 04:17:37 +0900

    Add life_flow_line.rb life_flow を controller から modele に移動

--- app/controllers/palmistry_controller.rb
+++ app/controllers/palmistry_controller.rb
@@ -101,22 +101,8 @@ class PalmistryController < ApplicationController
       @hand_shape_report = hand_shape.result_report(hand)
     end
 
-    @life_flow = params[:life_flow]
-    case params[:life_flow]
-    when '0' then
-      @life_flow = @uname + " さんは家族からの支援がある人です。<br>
-      困っている時や悩んでいる時は家族に相談することで解決することができるでしょう。<br>
-      なので、家族を大切にしましょう。<br>"
-    when '1' then
-      @life_flow = @uname + " さんは周りからの支援があるラッキーな人生です。<br>
-      困っている時や悩んでいる時は周りに相談することで解決することができるでしょう。<br>"
-    when '2' then
-      @life_flow = @uname + "さんは自分の道は自分で切り拓く人生を歩みます。<br>
-      困っている時や悩んでいる時は可能な限り自分で決断するようにして下さい。<br>
-      辛い時は周りに相談するとがあるかと思います。<br>
-      相手の意見をそのまま取り入れるのではなく、自分の中で一度整理をし、決断をして下さい。<br>
-      周りに流されないよう気をつけて下さい<br>"
-    end
+    life_flow_line = LifeFlowLine.new(@uname,@sex_human)
+    @life_flow = life_flow_line.result_flow(params[:life_flow])
   end
 
   def profile
