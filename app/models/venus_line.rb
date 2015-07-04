8847c23a899ba19c42fbdabf62661d619d05f8bb
himajin315 <himajin315@gmail.com>
2015-07-05 03:09:09 +0900

    Add models.venus.* to i18n

--- app/models/venus_line.rb
+++ app/models/venus_line.rb
@@ -5,19 +5,18 @@ class VenusLine
   end
 
   def result_virtuous
-    result = nil
     if @user.palm.venus_virtuous then
-      result = "さらに、" + @user.uname + " さんは周りからの信頼も厚いです。<br>
-      自分のことよりも周りへの貢献を優先させるような人です。<br>
-      とても素晴らしい人でしょう。<br>"
+      I18n.t('models.venus.virtuous', uname: @user.uname)
+    else
+      nil
     end
   end
 
   def result_kind_heart
-    result = nil
     if @user.palm.venus_kind_heart then
-      result = @user.uname + " さんは結婚後は子供を大切にします。<br>
-      家族のことを優先的に考え、家族を大切にする良い" + @sex_human +"になるタイプでしょう。<br>"
+      I18n.t('models.venus.kind_heart', uname: @user.uname, sex_human: @sex_human)
+    else
+      nil
     end
   end
 end
