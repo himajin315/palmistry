e355ff881e35f33b1970587e734a0adee3c8800d
himajin315 <himajin315@gmail.com>
2015-07-05 02:27:55 +0900

    Add modle.life_flow.* to i18n

--- app/models/life_flow_line.rb
+++ app/models/life_flow_line.rb
@@ -7,18 +7,11 @@ class LifeFlowLine
   def result_flow
     case @user.palm.life_flow
     when 0 then
-      result_flow = @user.uname + " さんは家族からの支援がある人です。<br>
-      困っている時や悩んでいる時は家族に相談することで解決することができるでしょう。<br>
-      なので、家族を大切にしましょう。<br>"
+      I18n.t('models.life_flow.family', uname: @user.uname)
     when 1 then
-      result_flow = @user.uname + " さんは周りからの支援があるラッキーな人生です。<br>
-      困っている時や悩んでいる時は周りに相談することで解決することができるでしょう。<br>"
+      I18n.t('models.life_flow.other', uname: @user.uname)
     when 2 then
-      result_flow = @user.uname + "さんは自分の道は自分で切り拓く人生を歩みます。<br>
-      困っている時や悩んでいる時は可能な限り自分で決断するようにして下さい。<br>
-      辛い時は周りに相談するとがあるかと思います。<br>
-      相手の意見をそのまま取り入れるのではなく、自分の中で一度整理をし、決断をして下さい。<br>
-      周りに流されないよう気をつけて下さい<br>"
+      I18n.t('models.life_flow.myself', uname: @user.uname)
     end
   end
 end
