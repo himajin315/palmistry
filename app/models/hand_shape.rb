196a1ac8008e96f219c585608f03a6cf20f7a1b1
himajin315 <himajin315@gmail.com>
2015-07-05 02:12:40 +0900

    Add model.hand_shape.* to i18n

--- app/models/hand_shape.rb
+++ app/models/hand_shape.rb
@@ -7,16 +7,14 @@ class HandShape
   def result_waste
     result = nil
     if @user.palm.hand_shape_waste then
-      result = @user.uname + "ただ、" + @user.uname + " さんは浪費家です。<br>
-      入った分だけ使ってしまうので、いくらお金があっても満足はしません。<br>"
+      I18n.t('models.hand_shape.waste', uname: @user.uname)
     end
   end
 
   def result_report
     result = nil
     if @user.palm.hand_shape_report then
-      result = "さらに、" + @user.uname + " さんは文章で伝える能力が高いです。<br>
-自分の考えを伝える時は文章にまとめて伝える方が良いでしょう。<br>"
+      I18n.t('models.hand_shape.report', uname: @user.uname)
     end
   end
 end
