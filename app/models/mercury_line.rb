df8ce4ecbcbfb7ecd4bd326c77b89bc0cad02e22
himajin315 <himajin315@gmail.com>
2015-07-05 02:43:36 +0900

    Add modle.mercury.money.* to i18n

--- app/models/mercury_line.rb
+++ app/models/mercury_line.rb
@@ -7,14 +7,11 @@ class MercuryLine
   def result_money
     case @user.palm.mercury_money
     when 0 then
-      result = @user.uname + " さんは現在お金に困っています。<br>
-      現在の収入に満足していません。<br>"
+      I18n.t('models.mercury.money.nothing', uname: @user.uname)
     when 1 then
-      result = @user.uname + " さんはお金は入ってきますが、すぐに出て行く状態です。<br>
-      貯金がしにくい状態のようです。<br>"
+      I18n.t('models.mercury.money.not_enough', uname: @user.uname)
     when 2 then
-      result = @user.uname + " さんはお金には困らない生活をしています。<br>
-      特にお金に困るようなこともなく、お金に縛られない生活ができています。<br>"
+      I18n.t('models.mercury.money.enough', uname: @user.uname)
     end
   end
 end
