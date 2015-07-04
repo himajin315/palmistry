7c3ac0cff633986b247bab6495b4f630a1b48c87
himajin315 <himajin315@gmail.com>
2015-07-05 02:47:09 +0900

    Add modle.moon.* to i18n

--- app/models/moon_line.rb
+++ app/models/moon_line.rb
@@ -5,18 +5,20 @@ class MoonLine
   end
 
   def result_like
-    result = nil
     if @user.palm.moon_like then
       result = "さらに、" + @user.uname + " さんは好かれやすい人でもあります。<br>
       接客業や営業など人と接することが多い仕事に就くのもよいでしょう。<br>"
+    else
+      nil
     end
   end
 
   def result_fatigue
-    result = nil
     if @user.palm.moon_fatigue then
       result = @user.uname + " さんは現在疲れがたまっているます。<br>
       休める時にはちゃんと休むようにして下さい。<br>"
+    else
+      nil
     end
   end
 end
