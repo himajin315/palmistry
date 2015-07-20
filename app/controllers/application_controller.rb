1962e545a7973e956f59e72b2594ff62003f148c
himajin315 <himajin315@gmail.com>
2015-07-20 22:05:19 +0900

    Add pdf.css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def render_to_pdf
-    search_wards = Array['.navbar-header','ul.nav.navbar-nav.navbar-right','.sns-button']
+    search_wards = Array['.navbar-header','ul.nav.navbar-nav.navbar-right','.sns-button','br']
     meta = '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'
     body = render_to_string formats: :html
     doc = Nokogiri::HTML(body)
+
+    # Remove not need tags
     search_wards.each do |ward|
       doc.css(ward).remove
     end
+
+    # Add .pdf-contaier
+    nodes_container = doc.css '.container'
+    nodes_container.wrap("<div id='pdf-container'></div>")
+
     html = meta + doc.to_html
     pdf  = WickedPdf.new.pdf_from_string(html)
   end
